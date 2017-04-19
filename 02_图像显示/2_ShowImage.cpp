947f7dc31cbeff9fcaa6c2774c9196eabdc1271d
jackros1022 <1347755930@qq.com>
2017-04-19 23:12:24 +0800

    4.16

--- 02_图像显示/2_ShowImage.cpp
+++ 02_图像显示/2_ShowImage.cpp
@@ -12,12 +12,12 @@
 #include <opencv2/opencv.hpp>  //头文件
 using namespace cv;  //包含cv命名空间
 
-void main( )
+int main( )
 {    
 	// 【1】读入一张图片，载入图像
 	Mat srcImage = imread("1.jpg");
 	// 【2】显示载入的图片
-	imshow("【原始图】",srcImage);
+	imshow("Picture",srcImage);
 	// 【3】等待任意按键按下
 	waitKey(0);
 }  
