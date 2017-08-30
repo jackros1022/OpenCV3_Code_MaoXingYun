b036315d88c8216729cbcc0bcd8b889672aaeddf
王冲 <1347755930@qq.com>
2017-08-30 17:12:46 +0800

    2017-08-30

--- 63_HoughCircles函数用法示例/63_HoughCircles.cpp
+++ 63_HoughCircles函数用法示例/63_HoughCircles.cpp
@@ -1,42 +1,26 @@
-//--------------------------------------【程序说明】-------------------------------------------
-//		程序说明：《OpenCV3编程入门》OpenCV2版书本配套示例程序63
-//		程序描述：HoughCircles函数用法示例
-//		开发测试所用操作系统： Windows 7 64bit
-//		开发测试所用IDE版本：Visual Studio 2010
-//		开发测试所用OpenCV版本：	2.4.9
-//		2014年06月 Created by @浅墨_毛星云
-//		2014年11月 Revised by @浅墨_毛星云
-//------------------------------------------------------------------------------------------------
-
-
-
-//---------------------------------【头文件、命名空间包含部分】----------------------------
-//		描述：包含程序所使用的头文件和命名空间
-//------------------------------------------------------------------------------------------------
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 using namespace cv;
+using namespace std;
 
 
-//-----------------------------------【main( )函数】------------------------------------------
-//		描述：控制台应用程序的入口函数，我们的程序从这里开始
-//-----------------------------------------------------------------------------------------------
 int main( )
 {
 	//【1】载入原始图、Mat变量定义   
-	Mat srcImage = imread("1.jpg");  //工程目录下应该有一张名为1.jpg的素材图
+	Mat srcImage = imread("data/4.jpg");  
 	Mat midImage,dstImage;//临时变量和目标图的定义
 
 	//【2】显示原始图
-	imshow("【原始图】", srcImage);  
+	//imshow("srcImage", srcImage);  
 
 	//【3】转为灰度图并进行图像平滑
 	cvtColor(srcImage,midImage, CV_BGR2GRAY);//转化边缘检测后的图为灰度图
-	GaussianBlur( midImage, midImage, Size(9, 9), 2, 2 );
+	GaussianBlur( midImage, midImage, Size(3, 3), 2, 2 );
 
 	//【4】进行霍夫圆变换
 	vector<Vec3f> circles;
-	HoughCircles( midImage, circles, CV_HOUGH_GRADIENT,1.5, 10, 200, 100, 0, 0 );
+	HoughCircles( midImage, circles, CV_HOUGH_GRADIENT,1, 10, 100, 25, 0, 50 );
+	//HoughCircles(midImage, circles, HOUGH_GRADIENT, 1, 5, 100, 25, 0, 50);
 
 	//【5】依次在图中绘制出圆
 	for( size_t i = 0; i < circles.size(); i++ )
@@ -51,7 +35,8 @@ int main( )
 	}
 
 	//【6】显示效果图  
-	imshow("【效果图】", srcImage);  
+	//imshow("midImage", midImage);
+	imshow("circles", srcImage);  
 
 	waitKey(0);  
 
